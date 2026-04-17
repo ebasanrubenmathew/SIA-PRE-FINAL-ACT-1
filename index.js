a3371607c48fa7ea2994e6c0f2010fce5c98f04f
ebasanrubenmathew <ebasanrubenmathew11@gmail.com>
2026-04-17 08:53:30 +0800

    tae

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-// run `node index.js` in the terminal
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+let students = [
+  { id: 1, name: 'student1', yearLevel: 1 },
+  { id: 2, name: 'student2', yearLevel: 2 },
+  { id: 3, name: 'student3', yearLevel: 3 },
+];
+
+app.get('/api/students', (req, res) => {
+  res.json(students);
+});
+
+app.status('/api/students', (req, res) => {
+  const { name, yearLevel } = req.body;
+
+  const newStudent = {
+    id: students.length ? students[students.length - 1].id + 1 : 1,
+    name,
+    yearLevel: parseInt(yearLevel)
+  };
+
+  students.push(newStudent);
+
+  res.status(201).json({
+    message: "Student added successfully",
+    student: newStudent
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
